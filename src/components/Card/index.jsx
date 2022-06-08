13fc41f94542c9f8503390a9537c1d5c35b0ae7f
magura13 <tiagolelis_@hotmail.com>
2022-06-08 20:48:38 -0300

    Remove transition

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,9 +4,9 @@ const Card = (props) => {
   return (
     <div className="Card">
       <img src={props.image} alt={`Foto de ${props.name}`} />
-      <h3>{props.name}</h3>
-      <p>Status: {props.status}</p>
-      <p>Species: {props.species}</p>
+      <h3 className="Text">{props.name}</h3>
+      <p className="Text">Status: {props.status}</p>
+      <p className="Text">Species: {props.species}</p>
     </div>
   );
 };
